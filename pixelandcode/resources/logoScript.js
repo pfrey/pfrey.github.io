cf56f8a6ba7a9cf46ed14303f4c07d2dfd52e6d6
pfrey <pattyfresonke@gmail.com>
2025-04-26 18:15:59 -0500

    Create logoScript.js

---
+++ pixelandcode/resources/logoScript.js
@@ -0,0 +1,24 @@
+<script>
+  const codeText = "code";
+  const codeEl = document.querySelector(".code");
+  let index = 0;
+
+  function typeCode() {
+    if (index < codeText.length) {
+      codeEl.textContent += codeText.charAt(index);
+      index++;
+      setTimeout(typeCode, 150); // speed of typing
+    } else {
+      codeEl.style.borderRight = "none";
+    }
+  }
+
+  // Start animation
+  window.onload = () => {
+    // First reveal PIXEL
+    const pixel = document.querySelector(".pixel");
+    pixel.style.animationDelay = "0.5s";
+    // Then type in code
+    setTimeout(typeCode, 1000);
+  };
+</script>
